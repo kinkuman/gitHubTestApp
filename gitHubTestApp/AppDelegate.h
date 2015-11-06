6900c8130c1b1358d27e3e78577f4f7ced2ed7e2
inagaki <inagaki-hiroyuki@mgt-net.com>
2015-11-06 20:28:25 +0900

    Initial Commit

---
+++ gitHubTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitHubTestApp
+//
+//  Created by user on 2015/11/06.
+//  Copyright (c) 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitHubTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitHubTestApp
+//
+//  Created by user on 2015/11/06.
+//  Copyright (c) 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
